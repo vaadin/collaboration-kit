ac04a7f6f9d0e79cd5b6edf981ba4599d857cd1a
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2025-08-18 11:34:07 +0300

    chore: fix the deploy script for 7.0 (#103)
    * chore: fix the deploy script for 7.0
    
    replace `master` with `main`
    replace the old repository name
    
    * remove flow-spring version setting code
    
    it is not needed any more, as vaadin-spring is coming from flow

--- scripts/prepareDeploy.sh
+++ scripts/prepareDeploy.sh
@@ -54,17 +54,17 @@ token=$2
 versionBase=`getBaseVersion $version`
 pomBase=`getBaseVersion $pomVersion`
 
-### Get the master branch version for CE
-masterPom=`curl -s "https://$token@raw.githubusercontent.com/vaadin/collaboration-engine-internal/master/pom.xml"`
+### Get the main branch version for CE
+masterPom=`curl -s "https://$token@raw.githubusercontent.com/vaadin/collaboration-kit/main/pom.xml"`
 masterMajorMinor=`echo "$masterPom" | grep '<version>' | cut -d '>' -f2 |cut -d '<' -f1 | grep "^$base" | head -1 | cut -d '-' -f1`
 
 ### Load versions file for this platform release
 branch=$versionBase
 if [ $branch = $masterMajorMinor ]
 then
-  branch=master
+  branch=main
 else
-  customPom=`curl -s "https://$token@raw.githubusercontent.com/vaadin/collaboration-engine-internal/$versionBase/pom.xml"`
+  customPom=`curl -s "https://$token@raw.githubusercontent.com/vaadin/collaboration-kit/$versionBase/pom.xml"`
   customMajorMinor=`echo "$customPom" | grep '<vaadin.component.version>' | cut -d '>' -f2 |cut -d '<' -f1 | grep "^$base" | head -1 | cut -d '-' -f1`
   branch=`getBaseVersion $customMajorMinor`
 fi
@@ -72,18 +72,14 @@ fi
 echo $branch
 
 versions=`curl -s "https://raw.githubusercontent.com/vaadin/platform/$branch/versions.json"`
-[ $? != 0 ] && branch=master && versions=`curl -s "https://raw.githubusercontent.com/vaadin/platform/$branch/versions.json"`
+[ $? != 0 ] && branch=main && versions=`curl -s "https://raw.githubusercontent.com/vaadin/platform/$branch/versions.json"`
 
 ### Check that current branch is valid for the version to release
-[ $branch != master -a "$versionBase" != "$pomBase" ] && echo "Incorrect pomVersion=$pomVersion for version=$version" && exit 1
+[ $branch != main -a "$versionBase" != "$pomBase" ] && echo "Incorrect pomVersion=$pomVersion for version=$version" && exit 1
 
 ### Compute flow version
 flow=`getPlatformVersion flow`
 flow=`getLatest flow $flow`
-
-### Compute spring version
-spring=`getPlatformVersion flow-spring`
-spring=`getLatest vaadin-spring $spring`
 
 ### Compute cdi version
 cdi=`getPlatformVersion flow-cdi`
@@ -98,7 +94,6 @@ echo "Setting version=$version to collaboration-engine-internal"
 mvn -B -q versions:set -DnewVersion=$version -DgenerateBackupPoms=false|| exit 1
 
 setPomVersion flow $flow
-setPomVersion vaadin.spring $spring
 setPomVersion flow.cdi $cdi
 setPomVersion vaadin.component $component || exit 1
 
@@ -107,6 +102,5 @@ setPomVersion vaadin.component $component || exit 1
 echo "##teamcity[setParameter name='ce.branch' value='$branch']"
 echo "##teamcity[setParameter name='maven.profile' value='$profile']"
 echo "##teamcity[setParameter name='flow.version' value='$flow']"
-echo "##teamcity[setParameter name='vaadin.spring.version' value='$spring']"
 echo "##teamcity[setParameter name='vaadin.cdi.version' value='$cdi']"
 echo "##teamcity[setParameter name='component.version' value='$component']"
